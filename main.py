722e441a4854c065544f6edeb4992012b1150494
Bekhzod Tokhirjonov <bekhzod.tokhirjonov@sap.com>
2023-02-15 13:24:54 +0500

    adds CORS middleware

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 from fastapi import FastAPI
 from database.database import Base, engine
 from routes import user_routes, category_routes, product_routes, transaction_routes
-
+from fastapi.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(engine)
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(user_routes.router)
 app.include_router(category_routes.router)
