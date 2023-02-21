d4bb71c8be9b3e2691b0098d9b6f1ae54cd73368
Bekhzod Tokhirjonov <bekhzod.tokhirjonov@sap.com>
2023-02-21 14:49:58 +0500

    adds auth login while getting the information of an admin

--- routes/user_routes.py
+++ routes/user_routes.py
@@ -6,6 +6,8 @@ from services.user_service.user_model import CreateUserModel, UpdateUserModel
 from services.user_service.user import create, get_list, get_by_id, delete, update
 from services.user_service.user_model import UserModel
 from database import database
+from database.oauth2 import get_current_user
+from services.user_service.user_model import UserModel
 
 router = APIRouter(
     prefix="/users",
@@ -16,20 +18,20 @@ get_db = database.get_db
 
 
 @router.get('/', status_code=status.HTTP_200_OK)
-def Get_list(session: Session = Depends(get_db)):
+def Get_list(session: Session = Depends(get_db), current_user: UserModel = Depends(get_current_user)):
     return get_list(session)
 
 
-# get, post, put, delete methods are working but disabled for security purposes for now
+@router.get('/{id}', status_code=status.HTTP_200_OK)
+def Get_by_id(id: UUID, session: Session = Depends(get_db), current_user: UserModel = Depends(get_current_user)):
+    return get_by_id(id, session)
 
-# @router.get('/{id}', status_code=status.HTTP_200_OK)
-# def Get_by_id(id: UUID, session: Session = Depends(get_db), current_user: UserModel = Depends(get_current_user)):
-#     return get_by_id(id, session)
 
 @router.post('/', status_code=status.HTTP_201_CREATED)
 def Create(request: CreateUserModel, session: Session = Depends(get_db)):
     return create(request, session)
 
+# put, delete methods are working but disabled for security purposes for now
 # @router.put('/{id}', status_code=status.HTTP_202_ACCEPTED) def Update(id: UUID, request: UpdateUserModel,
 # session: Session = Depends(get_db), current_user: UserModel = Depends(get_current_user)): return update(id,
 # request, session)
